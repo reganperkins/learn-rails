52a16006a285b361c3f27a22cf25e2ae60ac0dd4
Regan <reganlperkins@gmail.com>
2014-10-25 23:03:43 -0700

    creating my first MVC home page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+LearnRails::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -26,6 +26,20 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #added by regan information from enviromental variables
+  config.action_mailer.smtp_settings = { 
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"], 
+    authentication: "plain", 
+    enable_starttls_auto: true, 
+    user_name: ENV["GMAIL_USERNAME"], 
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+  # Send email in development mode.
+  config.action_mailer.perform_deliveries = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+LearnRails::Application.config.secret_key_base = 'b7dec83e4f7cd61c2226a4df2fcef3460bcaf4c63a16afdda8f6e332956ad49cc907052a403fb1b77e1b982da575cdade0c63f4bf41d2d4555283a2ee17e595e'
